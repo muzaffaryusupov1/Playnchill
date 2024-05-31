d9d8e29121daeb89f7e24f8519a434e8d1796164
Muzaffar Yusupov <themuzaffar7@gmail.com>
2024-05-31 22:43:44 +0500

    fixed

--- src/pages/categories/components/index.js
+++ src/pages/categories/components/index.js
@@ -1,5 +1,4 @@
 import CategoryProducts from "./CategoryProducts";
-import CategorySidebar from "./CategorySidebar";
 import FilterModal from "./FilterModal";
 
-export { CategoryProducts, CategorySidebar, FilterModal }
+export { CategoryProducts, FilterModal }
